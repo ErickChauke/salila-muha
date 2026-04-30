add8f6ab301359dcbe9c228ec92badfcde4ed39e
Erick Chauke <erickchauke0217@gmail.com>
2026-04-30 03:27:46 +0200

    fix socket transport - polling only

--- apps/frontend/src/lib/socket.ts
+++ apps/frontend/src/lib/socket.ts
@@ -2,4 +2,4 @@ import { io } from "socket.io-client";
 
 const socketUrl = process.env.NEXT_PUBLIC_API_URL || undefined;
 
-export const socket = io(socketUrl!, { autoConnect: false });
+export const socket = io(socketUrl!, { autoConnect: false, transports: ["polling"] });
